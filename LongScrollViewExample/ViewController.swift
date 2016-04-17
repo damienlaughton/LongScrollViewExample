4873815454042438c913d2cd09e37c8718a4cba5
Damien Laughton <dfplaughton@googlemail.com>
2016-04-17 12:16:34 +0100

    Initial Commit

---
+++ LongScrollViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LongScrollViewExample
+//
+//  Created by Damien Laughton on 17/04/2016.
+//  Copyright © 2016 Damien Laughton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
